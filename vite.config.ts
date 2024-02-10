ec3bba5b557fda65ba02d17886293397eea566ee
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2024-02-11 01:53:24 +0900

    chore: vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 import devServer from "@hono/vite-dev-server";
 import ssg from "@hono/vite-ssg";
-import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
     ssg(),
+    tsconfigPaths(),
     devServer({
       entry: "src/index.tsx",
     }),
