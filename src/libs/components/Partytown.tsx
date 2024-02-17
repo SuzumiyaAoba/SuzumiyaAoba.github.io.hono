d5756a44a38902fb2d1321e0eafdb40096da94e7
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2024-02-18 02:49:17 +0900

    fiX: partytown

--- src/libs/components/Partytown.tsx
+++ src/libs/components/Partytown.tsx
@@ -1,10 +1,11 @@
 import { partytownSnippet } from "@builder.io/partytown/integration";
+import { raw } from "hono/html";
 import type { FC } from "hono/jsx";
 
 export const Partytown: FC = () => {
   return (
     <>
-      <script>{partytownSnippet()}</script>
+      <script>{raw(partytownSnippet())}</script>
     </>
   );
 };
