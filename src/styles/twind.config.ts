02d5f8e1c4aa539c97c2fc11c6da58f060bff682
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2024-04-21 18:43:19 +0900

    fix: compile error

--- src/styles/twind.config.ts
+++ src/styles/twind.config.ts
@@ -1,14 +1,22 @@
-import { defineConfig } from "@twind/core";
+import { defineConfig, type BaseTheme, type PartialTheme } from "@twind/core";
 import presetAutoprefix from "@twind/preset-autoprefix";
 import presetTailwind from "@twind/preset-tailwind";
+
+// workaround: https://github.com/denoland/fresh/issues/1633#issuecomment-1680135412
+declare type ThemeConfig<Theme extends BaseTheme = BaseTheme> = PartialTheme<Theme> & {
+    extend?: PartialTheme<Theme>;
+};
 
 export const twindConfig = defineConfig({
   presets: [presetAutoprefix(), presetTailwind()],
   theme: {
-    fontFamily: {
-      "sans": ["IBM Plex Sans JP", "sans-serf"],
-      "mono":["M PLUS 1 Code", "monospace"],
-      "display": ["Pacifico", "system-ui"],
+    extend: {
+      fontFamily: {
+        sans: ["IBM Plex Sans JP", "sans-serf"],
+        serif: ["Shippori Mincho", "serif"],
+        mono:["M PLUS 1 Code", "monospace"],
+        display: ["Pacifico", "system-ui"],
+      },
     },
-  },
+  } as ThemeConfig,
 });
