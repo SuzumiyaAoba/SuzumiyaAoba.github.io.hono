afa5eed67940652d7a79f41063186493f727dbd4
eowiz <eowiz@proton.me>
2024-05-01 23:58:49 +0900

    feat: add navigation

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,16 +7,26 @@ export const Header: FC<{
   const titleCss = css`
     @apply font-display;
     @apply font-bold text-2xl;
-    @apply no-underline;
+    @apply no-underline hover:no-underline;
   `;
 
   return (
-    <header className="max-w-3xl mx-auto my-2 w-full">
-      <div>
+    <header className="flex max-w-3xl mx-auto my-2 w-full">
+      <div class="mr-12">
         <a class={titleCss} href="/">
           {props.siteName}
         </a>
       </div>
+      <nav className="hidden sm:block font-accent my-auto">
+        <ul class="flex gap-x-8 list-none">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/notes/">Notes</a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
